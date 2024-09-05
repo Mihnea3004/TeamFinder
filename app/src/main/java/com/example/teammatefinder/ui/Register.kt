651ad89a7f9a8b954a3e9f7087d14fc1a41687aa
Mihnea3004 <ion.mihneacristian@yahoo.ro>
2024-09-05 18:30:51 +0300

    login ui

--- app/src/main/java/com/example/teammatefinder/ui/Register.kt
+++ app/src/main/java/com/example/teammatefinder/ui/Register.kt
@@ -3,8 +3,6 @@ package com.example.teammatefinder.ui
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import com.example.teammatefinder.R
 
 class Register : AppCompatActivity() {
